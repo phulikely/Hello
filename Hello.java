d059abab88c3cd701f44f7d4eab8a5d50ff5804e
Phulikely <phulikely@gmail.com>
2019-04-03 09:44:49 +0700

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		for (int i = 0; i < 9; i++) 
+		 (int i = 0; i < 9; i++) 
 			System.out.println("Hello..." + i)
 		}
 	}
